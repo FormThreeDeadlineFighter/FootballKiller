fb106d5bac5718a40126c6a9339550c290a9a3f5
Poko0000 <pokopoko130000@gmail.com>
2026-02-08 02:11:11 +0800

    feat: boss model change

---
+++ Assets/Scripts/Characters/Enemy/Baseball/IBaseballState.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[Serializable]
+public class IBaseballState : IState
+{
+    float _stateEnterTime;
+    protected BaseballStateController _stateMachine;
+    protected Animator _animator;
+    protected PlayableDirector _director;
+    protected EnemyController _enemy;
+    protected bool IsAnimationComplete => _animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f;
+    protected float _stateDuration => Time.time - _stateEnterTime;
+
+    public void Initialize(BaseballStateController stateMachine, EnemyController enemy, Animator animator, PlayableDirector director)
+    {
+        _stateMachine = stateMachine;
+        _enemy = enemy;
+        _animator = animator;
+        _director = director;
+    }
+    // when enter state happen
+    public virtual void EnterState() 
+    {
+        _stateEnterTime = Time.time;
+    }
+    // when exit state happen
+    public virtual void ExitState() 
+    { 
+    
+    }
+    // state update, not using physics
+    public virtual void LogicUpdate() 
+    { 
+    
+    }
+    // state update, using physics
+    public virtual void PhysicsUpdate() 
+    { 
+    
+    }
+}
---
+++ Assets/Scripts/Characters/Enemy/EnemyContoller/EnemyController.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Energy Property")]
+    [SerializeField] private float _HP;
+    [SerializeField] float _moveSpeed;
+    [SerializeField] float _shieldHP;
+    [SerializeField] bool _invincible = false;
+    [SerializeField] private Elements currentElement = Elements.white; 
+    [SerializeField] Material[] ElementMaterials;  
+
+    [Header("Event System")]
+    [SerializeField] Renderer ElementsShow;
+    [SerializeField] GameEvent _gameEvent;
+    [SerializeField] BossEvent _bossEvent;
+
+    private Rigidbody _rb;
+    private AISensor _sensor;
+    private float _currentHP;
+    private float _currentShield;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _sensor = GetComponent<AISensor>();
+    }
+
+    void OnEnable()
+    {
+        _gameEvent.OnGameVictory += GameOver;
+        _gameEvent.OnGameDefeat += GameOver;
+        
+        _currentHP = _HP;
+        _shieldHP = 0;
+    }
+    void OnDisable()
+    {
+        _gameEvent.OnGameVictory -= GameOver;
+        _gameEvent.OnGameDefeat -= GameOver;
+    }
+    
+    private void BossHurt(float damage)
+    {
+        if(_invincible) return;      
+        if(_currentShield > 0) return;  
+        
+        if (_currentHP >= 0)
+        {
+            _currentHP -= damage;
+        }
+        if (_currentHP <= 0)
+        {
+            _currentHP = 0;
+            _gameEvent.EnemyDestory();
+            Destroy(gameObject);
+        }
+        
+        float hpPercentage = _currentHP/_HP;
+        _bossEvent.BossHPCahange(hpPercentage);
+    }
+    
+    private void ShieldHurt(float damage)
+    {
+        if(_invincible) return;       
+        
+        if (_shieldHP >= 0)
+        {
+            _shieldHP -= damage;
+        }
+        if (_shieldHP <= 0)
+        {
+            _shieldHP = 0;
+        }
+    }
+    
+    public void FaceToPlayer()
+    {
+        if(_sensor.Target == null) return;
+        Vector3 dir = _sensor.Target.transform.position - transform.position;
+        dir.y = 0;
+        Quaternion targetRot = Quaternion.LookRotation(dir);
+        _rb.transform.rotation = Quaternion.Slerp(transform.rotation,targetRot, 2 * Time.fixedDeltaTime);
+    }
+    public void SetVelocity(Vector3 vector3)
+    {
+        _rb.linearVelocity = vector3;
+        _rb.angularVelocity = vector3; 
+    }
+    
+    public void SwitchElement()
+    {
+        int num = (int)currentElement;
+        num = (num + 1)%2;
+        currentElement = (Elements)num;
+        ElementsShow.material = ElementMaterials[num];
+    } 
+    
+    public bool IsAttackable()
+    {
+        if(_currentHP > 0) return true;
+        else return false;
+    }
+    void OnDestroy()
+    {
+        //_gameEvent.EnemyDestory(this.gameObject);
+    }
+    private void GameOver()
+    {
+        Destroy(gameObject);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<IAttack>(out IAttack attack))
+        {
+            if(attack.Elements != currentElement) return;
+            BossHurt(attack.Damage);
+        }
+    }
+}
