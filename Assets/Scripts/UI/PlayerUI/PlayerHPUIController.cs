2399cba37588f14983fd3ecc8329525d4ac59ad1
Poko0000 <pokopoko130000@gmail.com>
2026-03-09 21:37:05 +0800

    feat: delay hp

--- Assets/Scripts/UI/PlayerUI/PlayerHPUIController.cs
+++ Assets/Scripts/UI/PlayerUI/PlayerHPUIController.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHPUIController : MonoBehaviour
 {
-    [SerializeField] Image HPUI;
+    [SerializeField] Image _HPUI;
+    [SerializeField] Image _backgroundHP;
+    [SerializeField] float _delayTime = 0.5f;
     [SerializeField] PlayerEvent _playerEvents;
+    Coroutine hpCoroutine;
     void OnEnable()
     {
         _playerEvents.OnPlayerHPChange += HPUICahnge;
@@ -17,6 +21,33 @@ public class PlayerHPUIController : MonoBehaviour
     private void HPUICahnge(float value)
     {
         value = value * 0.9f + 0.1f;
-        HPUI.fillAmount = value;
+        _HPUI.fillAmount = value;
+        IEnumerator enumerator = HPDelayReduce(value);
+        
+        if (hpCoroutine != null)
+        {      
+            StopCoroutine(hpCoroutine);
+        }
+        hpCoroutine = StartCoroutine(enumerator);
+    }
+    
+    IEnumerator HPDelayReduce(float target)
+    {
+        yield return new WaitForSeconds(0.5f);
+        
+        float start = _backgroundHP.fillAmount;
+        float time = 0;
+
+        while (time < _delayTime)
+        {
+            time += Time.deltaTime;
+
+            _backgroundHP.fillAmount = Mathf.MoveTowards(start, target, time / _delayTime);
+
+            yield return null;
+        }
+
+        _backgroundHP.fillAmount = target;
+        hpCoroutine = null;
     }
 }
