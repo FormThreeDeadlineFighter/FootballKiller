9c11bc5e6d4a8d1ba71f838c9dd2aab7bfb43e12
Poko0000 <pokopoko130000@gmail.com>
2026-03-14 18:17:53 +0800

    feat: enemy on hit effect

---
+++ Assets/Scripts/Characters/Enemy/Baseball/BaseballController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BaseballController : MonoBehaviour
+{
+    [SerializeField] float _collisionforwardDistance;
+    [SerializeField] ParticleSystem _onHitEffect;
+    [SerializeField] BossEvent _bossEvent;
+
+    void OnEnable()
+    {
+        _bossEvent.OnBossHPCahange += OnHurtEffect;
+    }
+    public void CollisionMoveTrigger()
+    {
+        transform.position = transform.position + transform.forward * _collisionforwardDistance;
+    }
+    
+    private void OnHurtEffect(float value)
+    {
+        _onHitEffect.Play();
+    }
+}
